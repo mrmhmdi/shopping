c215fd490b5c55c337ad3a81aea4841ecb3b39e5
Mohammadreza Mohammadi <mr.mhdi1995@gmail.com>
2021-10-01 00:07:28 +0330

    give some style, and update shop page items

--- src/components/preview-shopitems/preview-shopitems.component.jsx
+++ src/components/preview-shopitems/preview-shopitems.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Shopitemcard from '../../components/shopitem-card/shopitem-card.component';
+import './preview-shopitems.style.scss'
 
 const Previewshoopitems = ({title, items}) => (
-    <div className='shopitem'>
+    <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
-                items.filter((item, index) => index <4).map(item => (
-                    <div key={item.id}>{item.name}</div>
+                items.filter((item, index) => index <4).map(({id, ...otheritemprops}) => (
+                    <Shopitemcard key={id} {...otheritemprops}/>
                 ))
             }
         </div>
